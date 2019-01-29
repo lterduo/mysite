f673b9db9c093bd20cbdcc6c9cbaa0b18bc57588
lterduo <lterduo@qq.com>
2019-01-29 14:36:46 +0800

    add kvm

--- flutter/beautiful_girls/python/crawler_girls.py
+++ flutter/beautiful_girls/python/crawler_girls.py
@@ -2,11 +2,13 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import json
+from pymongo import MongoClient
+
 headers = {
     'accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 }
-urls = ['https://www.pexels.com/?page={}'.format(str(i)) for i in range(1, 3)]
+urls = ['https://www.pexels.com/?page={}'.format(str(i)) for i in range(1, 10)]
 photos = []
 for url in urls:
     wb_data = requests.get(url, headers=headers)
@@ -16,21 +18,25 @@ for url in urls:
         if img.get('data-big-src') != None:
             temp = img.get('data-big-src')
             print('***************'+temp)
-            # if re.findall('.*jpeg', temp):
-            #     temp = re.findall('.*jpeg', temp)[0]
             photos.append({"img":temp})
 
-with open('./python/photo.json', 'w') as f:
-    json.dump(photos, f, sort_keys=False , indent=4, separators=(',', ': '), ensure_ascii=False)
-# if photo_name:
-#     fp = open(path + photo_name[0], 'wb')
-#     fp.write(data.content)
-#     fp.close()
+#mongoDB
+client = MongoClient()
+db_girls = client.girls
+collection_imgs = db_girls.imgs
+# imgs = {'img':'1'}
+# imgs_insert = collection_imgs.insert_one(imgs)
+# imgs_remove = collection_imgs.remove()
+# imgs_update = collection_imgs.update(
+#     {'img':'test'},
+#     {'$set':
+#         {'age':18}}
+# )
+for i in photos:
+    imgs_find_one = collection_imgs.find_one(i)
+    if imgs_find_one :
+        continue
+    imgs_insert = collection_imgs.insert(i)
 
-# url = 'http://www.imagebam.com/gallery/7oyt77oqulbsotmggokeykvqato91k59'
-# get_info(url)
-# url_in = 'http://www.imagebam.com/image/2723a81102983184'
-# web_data = requests.get(url_in, headers=headers)
-# soup = BeautifulSoup(web_data.text, 'lxml')
-# big_jpg = soup.select('.image')
-# print(soup)
+# with open('./python/photo.json', 'w') as f:
+#     json.dump(photos, f, sort_keys=False , indent=4, separators=(',', ': '), ensure_ascii=False)
