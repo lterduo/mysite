83cf6719b88f1b859f1dbf0ac5380df3a3e51e9e
lterduo <lterduo@qq.com>
2021-02-26 00:39:06 +0800

    aliy

--- caspd-keti/vue-admin/src/components/home/HomeMain.vue
+++ caspd-keti/vue-admin/src/components/home/HomeMain.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
     <div>
-      ueditor
-      <vue-ueditor-wrap v-model="text" :config="editorConfig"></vue-ueditor-wrap>
+      <!-- ueditor
+      <vue-ueditor-wrap v-model="text" :config="editorConfig"></vue-ueditor-wrap> -->
     </div>
   </div>
 
 </template>
 
 <script>
-import VueUeditorWrap from 'vue-ueditor-wrap'
+// import VueUeditorWrap from 'vue-ueditor-wrap'
 
 export default {
-  components: {
-    VueUeditorWrap
-  },
+  // components: {
+  //   VueUeditorWrap
+  // },
   data () {
     return {
       text: '',
-      editorConfig: {
-        autoHeightEnabled: false, //编译器不自动被内容撑高
-        initialFrameHeight: 350, //初始容器高度
-        initialFrameWith: 900,
-        // serverUrl: "/api/ueditor/net/controller.ashx", //上传文件地址
-        UEDITOR_HOME_URL: "/static/UEditor/", //UEditor资源文件的存放路径
-      },
+      // editorConfig: {
+      //   autoHeightEnabled: false, //编译器不自动被内容撑高
+      //   initialFrameHeight: 350, //初始容器高度
+      //   initialFrameWith: 900,
+      //   // serverUrl: "/api/ueditor/net/controller.ashx", //上传文件地址
+      //   UEDITOR_HOME_URL: "/static/UEditor/", //UEditor资源文件的存放路径
+      // },
     }
   },
 
